0ed6ada5ad03918d88678a495c5c55ffbed2d7f6
sherrinfordh23 <thanhantrinh2015@gmail.com>
2022-04-12 15:38:13 -0400

    Modified city class for a little bit

--- classes/City.cls.php
+++ classes/City.cls.php
@@ -1,1 +1,14 @@
 <?php
+class City{
+    private $cityName;
+    private $cityId;
+    
+    
+    function __construct($cityId=null,$cityName=null){
+        $this->cityId=$cityId;
+        $this->cityName=$cityName;
+    }
+    
+    
+    
+}
