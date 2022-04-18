11aace02c908d151edd09e0ed7b1b7036e213de4
abu-ullah <abu.ullah03@gmail.com>
2022-04-17 21:00:14 -0400

    Adding Trip class

--- classes/Trip.cls.php
+++ classes/Trip.cls.php
@@ -1,10 +1,90 @@
 <?php
 
-
 class Trip {
     
     private $tripId;
-    private $userId;
+    private $user;
+    private $city;
+    private $listOfActivities;
+    
+    function __construct($tripId = null, $user = null, $city = null, $listOfActivities = null)
+    {
+        $this->tripId=$tripId;
+        $this->user=$user;
+        $this->city=$city;
+        $this->listOfActivities=$listOfActivities;
+    }
+    
+    
+    /**
+     * @return mixed
+     */
+    public function getTripId()
+    {
+        return $this->tripId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCityId()
+    {
+        return $this->cityId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getListOfActivities()
+    {
+        return $this->listOfActivities;
+    }
+
+    /**
+     * @param mixed $tripId
+     */
+    public function setTripId($tripId)
+    {
+        $this->tripId = $tripId;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @param mixed $cityId
+     */
+    public function setCityId($cityId)
+    {
+        $this->cityId = $cityId;
+    }
+
+    /**
+     * @param mixed $listOfActivities
+     */
+    public function setListOfActivities($listOfActivities)
+    {
+        $this->listOfActivities = $listOfActivities;
+    }
+
+    
+    public function displayTrips($connection)
+    {
+        
+    }
     
     
     
